bd3e1eb99bfe7be3b87a5a4571c4d11e6a29a49c
tg0xff <tg0xff@proton.me>
2024-07-31 17:08:28 -0600

    Define set() method

--- hashmap.js
+++ hashmap.js
@@ -2,6 +2,7 @@ export default class HashMap {
   constructor() {
     this.capacity = 16;
     this.loadFactor = 0.75;
+    this.buckets = [];
   }
   hash(key) {
     let hashCode = 0;
@@ -10,5 +11,20 @@ export default class HashMap {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
     }
     return hashCode;
+  }
+  updateBucket(index, value) {
+    if (index < 0 || index >= this.capacity) {
+      throw new Error("Trying to access index out of bound");
+    }
+    this.buckets[index] = value;
+  }
+  set(key, value) {
+    const hashCode = this.hash(key);
+    const data = {
+      key,
+      value,
+      next: null,
+    };
+    this.updateBucket(hashCode, data);
   }
 }
