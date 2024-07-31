e8885f77d9a1325058c9baafc70fd6629aee4c36
tg0xff <tg0xff@proton.me>
2024-07-31 15:20:56 -0600

    Copy TOP's hash function

---
+++ hashmap.js
@@ -0,0 +1,10 @@
+export default class HashMap {
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+    return hashCode;
+  }
+}
