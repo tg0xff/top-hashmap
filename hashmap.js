04896e65cfbda59f273879fe36f7d35ee3af8e1d
tg0xff <tg0xff@proton.me>
2024-08-01 15:22:23 -0600

    Add HashSet class block

--- hashmap.js
+++ hashmap.js
@@ -121,3 +121,18 @@ export default class HashMap {
     entries.forEach((entry) => this.#addOrUpdateKey(...entry));
   }
 }
+
+class HashSet {
+  buckets = [];
+  #capacity = 16;
+  #loadFactor = 0.75;
+  #length = 0;
+  #hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+    return hashCode % this.#capacity;
+  }
+}
