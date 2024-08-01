1aa2334017ad9e677fb725b3a30cacb55a4ac55c
tg0xff <tg0xff@proton.me>
2024-07-31 21:13:47 -0600

    Add main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+import HashMap from "./hashmap.js";
+
+const hashMap = new HashMap();
+
+hashMap.set("apple", "red");
+hashMap.set("banana", "yellow");
+hashMap.set("carrot", "orange");
+hashMap.set("dog", "brown");
+hashMap.set("elephant", "gray");
+hashMap.set("frog", "green");
+hashMap.set("grape", "purple");
+hashMap.set("hat", "black");
+hashMap.set("ice cream", "white");
+hashMap.set("jacket", "blue");
+hashMap.set("kite", "pink");
+hashMap.set("lion", "golden");
